869df62195df6dd6c7f969f71200c260cbfefd74
kkotdari <kkotdari@gmail.com>
2023-07-10 22:03:22 +0900

    Update UserService.java

--- src/main/java/com/kkot/blog/service/UserService.java
+++ src/main/java/com/kkot/blog/service/UserService.java
@@ -1,8 +1,10 @@
 package com.kkot.blog.service;
 
+import com.kkot.blog.model.RoleType;
 import com.kkot.blog.model.User;
 import com.kkot.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,14 +13,15 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
     @Transactional
-    public int join(User user){
-        try {
-            userRepository.save(user);
-            return 1;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return -1;
+    public void join(User user){
+        String rawPassword = user.getPassword();
+        String encPassword = encoder.encode(rawPassword); // password를 해시 암호화
+        user.setPassword(encPassword);
+        user.setRole(RoleType.USER);
+        userRepository.save(user);
     }
 }
