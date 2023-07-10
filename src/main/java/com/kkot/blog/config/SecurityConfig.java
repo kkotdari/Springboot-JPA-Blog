20ae0a35f4b9100b1ffe6dfbb6ecbf639429a783
kkotdari <kkotdari@gmail.com>
2023-07-11 00:58:23 +0900

    Update SecurityConfig.java

--- src/main/java/com/kkot/blog/config/SecurityConfig.java
+++ src/main/java/com/kkot/blog/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.kkot.blog.config;
 
+import com.kkot.blog.config.auth.PrincipalDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -14,19 +16,19 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity // 시큐리티 필터 등록 == 스프링 시큐리티의 설정을 이 파일에서 수행
 @EnableGlobalMethodSecurity(prePostEnabled = true) // 특정 주소로 접근을 하면 권한 및 인증을 미리 체크
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private PrincipalDetailsService principalDetailsService;
+
     @Bean // IoC: 리턴값을 스프링이 관리함
     public BCryptPasswordEncoder encodePwd() {
-        String encPassword = new BCryptPasswordEncoder().encode("1234");
         return new BCryptPasswordEncoder();
     }
 
     // 시큐리티가 대신 로그인할 때 password를 가로채는데 해당 password가 뭘로 해시되어 회원가입이 되었는지 알아야
     // 같은 해시로 암호화해서 DB에 있는 해시값과 비교할 수 있음
-
-
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(null).passwordEncoder(encodePwd());
+        auth.userDetailsService(principalDetailsService).passwordEncoder(encodePwd());
     }
 
     @Override
@@ -34,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
             .csrf().disable() // csrf 토큰 검증 비활성화 (개발 중 테스트 시 걸어두는 게 좋음)
             .authorizeRequests() // 요청 인증하기
-                .antMatchers("/", "/auth/**", "/js/**", "/image/**") // 이 경로로 들어오는 요청에 대해
+                .antMatchers("/", "/auth/**", "/js/**", "/image/**", "/dummy/**") // 이 경로로 들어오는 요청에 대해
                 .permitAll() // 허용함
                 .anyRequest() // 그 밖의 경로의 요청은
                 .authenticated() // 인증되어야함
