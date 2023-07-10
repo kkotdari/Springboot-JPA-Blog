7823064d7e6a6267cb9222ee6594862676aeb3b1
kkotdari <kkotdari@gmail.com>
2023-07-10 19:50:04 +0900

    Update user.js

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -6,7 +6,6 @@ let index = {
         });
     },
     save: function(){
-//        alert('user의 save함수 호출됨');
         let data = {
             username: $("#username").val(),
             password: $("#password").val(),
@@ -14,14 +13,14 @@ let index = {
         };
         $.ajax({
             type: "POST",
-            url: "/blog/api/user",
+            url: "/auth/join-proc",
             data: JSON.stringify(data),
             contentType: "application/json; charset=utf-8", // 요청 body 데이터 타입(MIME 방식)
             dataType: "json" // 서버에서 응답하는 문자열의 형태가 json이면 javascript object로 변경해 줌.
         }).done(function(res){
             alert("회원가입이 완료되었습니다.");
             console.log(res);
-            location.href="/blog";
+            location.href="/";
         }).fail(function(error){
             alert("JSON.stringify(error)")
         });
