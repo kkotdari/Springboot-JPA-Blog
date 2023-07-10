df9be1643de0e686aeba35a836245a3927bee113
kkotdari <kkotdari@gmail.com>
2023-07-11 06:07:55 +0900

    Update user.js

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -4,6 +4,9 @@ let index = {
         $("#btn-save").on("click", ()=>{
             this.save();
         });
+        $("#btn-update").on("click", ()=>{
+            this.update();
+        });
     },
     save: function(){
         let data = {
@@ -19,6 +22,26 @@ let index = {
             dataType: "json" // 서버에서 응답하는 문자열의 형태가 json이면 javascript object로 변경해 줌.
         }).done(function(res){
             alert("회원가입이 완료되었습니다.");
+            console.log(res);
+            location.href="/";
+        }).fail(function(error){
+            alert("JSON.stringify(error)");
+        });
+    },
+    update: function(){
+        let data = {
+            id: $("#id").val(),
+            password: $("#password").val(),
+            email: $("#email").val()
+        };
+        $.ajax({
+            type: "PUT",
+            url: "/user/",
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8", // 요청 body 데이터 타입(MIME 방식)
+            dataType: "json" // 서버에서 응답하는 문자열의 형태가 json이면 javascript object로 변경해 줌.
+        }).done(function(res){
+            alert("회원 정보 수정이 완료되었습니다.");
             console.log(res);
             location.href="/";
         }).fail(function(error){
